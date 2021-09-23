0f86bca377dfb345916ea6b062c6dc271bc0b15e
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-09-23 11:17:38 -0500

    update

--- client/connections.js
+++ client/connections.js
@@ -12,23 +12,55 @@ const readData = async () => {
     // const userInput = document.querySelector(".find-input").value;
     // console.log(userInput)
     const url = "http://localhost:3021/viewHotels";
-    const seedData = await fetch(url, {
-        method: "GET",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    let seedInfo = await seedData.json();
+    // // const seedData = await fetch(url, {
+    //     method: "GET",
+    //     mode: "cors",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    // });
+    // let seedInfo = await seedData.json();
     // console.log(seedInfo)
-   
+    const seedInfo = [
+        {
+         createdAt: "2021-09-22T00:27:13.594Z",
+         hotelAddress: "4375 Admiralty Way, Marina Del Rey ",
+         hotelImg: "https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/partnerimages/31/82/318244150.jpeg",
+         hotelName: "The Ritz-Carlton, Marina del Rey",
+         hotelState: "CA",
+         id: 8,
+         updatedAt: "2021-09-22T00:27:13.594Z",
+         
+        },
+        {
+            createdAt: "2021-09-22T00:27:13.594Z",
+            hotelAddress: "4375 Admiralty Way, Marina Del Rey ",
+            hotelImg: "https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/partnerimages/31/82/318244150.jpeg",
+            hotelName: "The Ritz-Carlton, Marina del Rey",
+            hotelState: "CA",
+            id: 8,
+            updatedAt: "2021-09-22T00:27:13.594Z",
+            
+           },
+           {
+            createdAt: "2021-09-22T00:27:13.594Z",
+            hotelAddress: "4375 Admiralty Way, Marina Del Rey ",
+            hotelImg: "https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/partnerimages/31/82/318244150.jpeg",
+            hotelName: "The Ritz-Carlton, Marina del Rey",
+            hotelState: "CA",
+            id: 8,
+            updatedAt: "2021-09-22T00:27:13.594Z",
+            
+           },
+    ]
     for (item of seedInfo) {
         const id = item.id;
         const name = item.hotelName;
         const address = item.hotelAddress;
         const image = item.hotelImg;
         
-        // const hotelContainer = document.createElement("div");
+        const hotelContainer = document.createElement("div");
+        hotelContainer.className = "hotel-card"
         const idNumber = document.createElement("h2");
         const hotelName = document.createElement("h1");
         const hotelImg = document.createElement("img");
@@ -45,7 +77,8 @@ const readData = async () => {
         hotelName.innerHTML = name;
         hotelImg.src = image;
         hotelAddress.innerHTML = address;
-        infoContainer.append(hotelName, hotelImg, hotelAddress, idNumber, resStart, resEnd,reservationButton)
+        hotelContainer.append(hotelName, hotelImg, hotelAddress, idNumber, resStart, resEnd,reservationButton)
+        infoContainer.append(hotelContainer)
 
         
         
